83af3459de6ff51856412e0dc689eee9dc694e78
Kristoupher <kri.arregui@gmail.com>
2022-10-19 00:05:50 +0200

    création de l'application

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className="b3 flex-fill">
+            Content
+        </div>
+    );
+};
+
+export default Content;
